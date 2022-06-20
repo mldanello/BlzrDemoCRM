e0c32a6a5f59858dd5a1d1bdda5f317162fb33ab
mdanello <mdanello26@gmail.com>
2022-06-20 22:29:09 +0000

    Add Controller

---
+++ BlzrDemoCRM/Server/Controllers/ContactController.cs
@@ -0,0 +1,55 @@
+﻿using BlzrDemoCRM.Server.Interfaces;
+using BlzrDemoCRM.Shared.Models;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlzrDemoCRM.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly IContact _IContact;
+        public ContactController(IContact iContact)
+        {
+            _IContact = iContact;
+        }
+
+        [HttpGet]  
+        public async Task<List<Contact>> Get()
+        {
+            return await Task.FromResult(_IContact.ListContacts());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            Contact contact = _IContact.GetContactById(id);
+            if (contact != null)
+            {
+                return Ok(contact);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public void Post(Contact contact)
+        {
+            _IContact.AddContact(contact);
+        }
+
+        [HttpPut]
+        public void Put(Contact contact)
+        {
+            _IContact.UpdateContact(contact);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _IContact.RemoveContact(id);
+            return Ok();
+        }
+    }
+}
