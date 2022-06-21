aec931b0380103e1fbc8d7cab3d2de02460f3efc
mdanello <mdanello26@gmail.com>
2022-06-21 00:48:53 +0000

    Clean Build

--- BlzrDemoCRM/Shared/Models/Contact.cs
+++ BlzrDemoCRM/Shared/Models/Contact.cs
@@ -12,7 +12,7 @@ namespace BlzrDemoCRM.Shared.Models
         public string Name { get; set; } = string.Empty;
         public string? Address { get; set; }
         public string? Phone { get; set; }
-        public int Age { get; set; }
+        public string? Age { get; set; }
 
     }
 }
---
+++ SQL/SeedSampleData.sql
@@ -0,0 +1,20 @@
+Insert into dbo.Contact	
+([Name], [Address], [Phone], [Age])
+Values
+('Micheal Mouse', '1001 Magic Way', '(123)444-5545', 17);
+
+Insert into dbo.Contact	
+([Name], [Address], [Phone], [Age])
+Values
+('Don L. Duck', '1201 West Space Mt. Ave', '(111)222-6657', 27);
+
+
+Insert into dbo.Contact	
+([Name], [Address], [Phone], [Age])
+Values
+('Foghorn Legion', '1341 Melody Road', '(889)6544-1123', 37);
+
+
+
+
+Select * From Contact;
