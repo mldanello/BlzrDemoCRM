91f9805d93f4e83deaf583803ca01d9eb9188221
mdanello <mdanello26@gmail.com>
2022-06-21 12:06:11 -0400

    Minor Code Clean Up

--- BlzrDemoCRM/Server/Data/DatabaseContext.cs
+++ BlzrDemoCRM/Server/Data/DatabaseContext.cs
@@ -14,29 +14,5 @@ namespace BlzrDemoCRM.Server.Data
         }
 
         public virtual DbSet<ContactEntity> Contacts { get; set; } = null!;
-
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-
-        //    modelBuilder.Entity<ContactEntity>(entity =>
-        //    { 
-                
-        //        entity.ToTable("Contact");
-        //        entity.Property(e => e.Id).HasColumnName("Id");
-        //        entity.Property(e => e.Name)
-        //            .HasMaxLength(100)
-        //            .IsUnicode(false);
-        //        entity.Property(e => e.Address)
-        //            .HasMaxLength(500)
-        //            .IsUnicode(false);
-        //        entity.Property(e => e.Phone)
-        //            .HasMaxLength(50)
-        //            .IsUnicode(false);
-
-        //    });
-        //    OnModelCreatingPartial(modelBuilder);
-        //}
-        //partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
-
     }
 }
